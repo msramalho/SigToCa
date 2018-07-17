f603f25fe06c7a87ffb10c6c5cc7fee03bf5ebf2
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-07-17 14:42:19 +0100

    Feature implemented

--- js/scripts/profile_pic.js
+++ js/scripts/profile_pic.js
@@ -1,14 +1,36 @@
-function overrideEditBtn() {
+/**
+ * Saves the profile picture
+ * 
+ * @param {<img>} img The img DOM element to download
+ */
+function savePic(img) {
+    // Create an empty canvas element
+    var canvas = document.createElement("canvas");
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
+
+    // Copy the image contents to the canvas
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    // save
+    canvas.toBlob(function(blob) {
+        saveAs(blob, "pic.jpeg");
+    }, "image/jpeg", 1);
+}
+
+/**
+ * Replaces the edit button hover the profile picture and adds a button to download the image
+ * Both buttons are placed below the image frame
+ */
+function updateDOM() {
     // div that contains the profile picture and edit button
     var $divEl = $('#conteudoinner .estudante-foto');
-    var $imgURL = $divEl.children('img').prop('src');
-    var $editURL = $divEl.find('a').prop('href');
+    // the url to sigarra's editing page
+    var editURL = $divEl.find('a').prop('href');
 
     // remove the edit button
     $divEl.children('div').remove('div .estudante-foto-editar');
-
-    // add two buttons (edit and download)
-    $divEl.add('div').addClass('btn-group').prop('role', 'group');
     
     // create new DOM elements
     var $divContainer = $('<div class="btn-group" role="group" aria-label="Basic example"/>');
@@ -16,6 +38,14 @@ function overrideEditBtn() {
     var $downloadBtn = $('<button type="button" class="btn btn-outline-dark">Save</button>');
 
     // add events for both both buttons
+    $editBtn.click(function () {
+        window.location.href = editURL;
+    });
+
+    $downloadBtn.click(function () {
+        var $imgEl = $('#conteudoinner .estudante-foto').children('img');
+        savePic($imgEl[0]); // [0] converts from jquery object to DOM element
+    });
 
     // start appending
     $divEl.append(
@@ -23,5 +53,5 @@ function overrideEditBtn() {
 }
 
 $(document).ready(function () {
-    overrideEditBtn();
+    updateDOM();
 });
