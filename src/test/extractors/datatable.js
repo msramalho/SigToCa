572583ef1123f0d3683fad9d3a1160a152f1dbfb
Fabio Gaspar <fabiodanielreis@hotmail.com>
2021-03-10 12:20:39 +0000

    test: Add tests for datatable extractor

---
+++ src/test/extractors/datatable.js
@@ -0,0 +1,68 @@
+//var assert = require('chai').assert;
+
+describe("Datatables", () => {
+
+    describe("Test all valid tables", () => {
+        let dt = null;
+
+        /**
+         * Loads sample file with multiple valid tables
+         */
+        before(() => {
+            return new Promise((resolve) =>
+                updatejQueryContext("test/pages/datatables/all-valid.html").then(() => {
+                    dt = new DataTable();
+                    dt.attachIfPossible();
+                    resolve();
+                }));
+        })
+
+        /**
+         * Ensure the extractor finds all possible tables
+         */
+        it("Find all candidate HTML tables", () => {
+            chai.assert.isArray(dt.tables, "tables is array");
+            chai.assert.equal(dt.tables.length, 4, "Failed to detect all candidate tables");
+        })
+
+        /**
+         * Test the table validator utility function
+         */
+        it("Test table validator function", () => {
+            for (const t of dt.tables) {
+                chai.assert.strictEqual(true, dt.validTable($(t)), "Failed validation on " + t.id);
+            }
+        });
+
+        /**
+         * Check DataTable lib loads correctly on all candidate tables
+         */
+        it("Check DataTable loads correctly on all candidates", () => {
+            /** if DataTables lib applied on table correctly, then the <table> node has a new class "dataTable" */
+            for (const t of dt.tables) {
+                chai.assert.include(t.getAttribute("class"), "dataTable", "DataTable not loaded");
+            }
+        })
+
+        /**
+         * Check if the <table> node is wrapped with a <div>, created to facilitate CSS customization
+         */
+        it("Check table is wrapped", () => {
+            // Table node must be wrapped on a div.SigTools__dt__table
+            for (const t of dt.tables) {
+                chai.assert.equal(t.parentElement.getAttribute("class"), "SigTools__dt__table", "DataTable not wrapped");
+            }
+        })
+
+        /**
+         * Check if <table> node and all DataTable elements (filters, buttons) are wrapped in a <div> element
+         */
+        it("Check table and DataTable are wrapped", () => {
+            // Table node and parent DataTable nodes must be wrapped on a div.SigTools__dt__table
+            for (const t of dt.tables) {
+                chai.assert.equal(t.parentElement.parentElement.getAttribute("class"), "SigTools__dt", "DataTable not wrapped");
+            }
+        })
+    })
+
+})
