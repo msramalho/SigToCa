605053b0592c562216786641c3fdb0371481d90f
Fabio Gaspar <fabiodanielreis@hotmail.com>
2021-03-10 22:32:33 +0000

    test: Fix leaks warnings by Mocha
    - Instead of using global variables, initialized on 'before' hook and
      available on the unit tests, it uses the Mocha context (this) to store
      such variables
    - As a result, arrow functions are dropped because they bind the
      context. Moreover, Mocha discourages them: https://mochajs.org/#arrow-functions

--- src/test/extractors/datatable.js
+++ src/test/extractors/datatable.js
@@ -1,17 +1,13 @@
-//var assert = require('chai').assert;
-
-describe("Datatables", () => {
-    describe("Out-of-the-box valid tables", () => {
-        let dt = null;
-
+describe("Datatables", function () {
+    describe("Out-of-the-box valid tables", function () {
         /**
          * Loads sample file with multiple valid tables
          */
-        before(() => {
+        before(function () {
             return new Promise((resolve) =>
                 updatejQueryContext("test/pages/datatables/all_valid.html").then(() => {
-                    dt = new DataTable();
-                    dt.attachIfPossible();
+                    this.dt = new DataTable();
+                    this.dt.attachIfPossible();
                     resolve();
                 })
             );
@@ -20,26 +16,26 @@ describe("Datatables", () => {
         /**
          * Ensure the extractor finds all possible tables
          */
-        it("Find all candidate HTML tables", () => {
-            chai.assert.isArray(dt.tables, "tables is array");
-            chai.assert.equal(dt.tables.length, 3, "Failed to detect all candidate tables");
+        it("Find all candidate HTML tables", function () {
+            chai.assert.isArray(this.dt.tables, "tables is array");
+            chai.assert.equal(this.dt.tables.length, 3, "Failed to detect all candidate tables");
         })
 
         /**
          * Test the table validator utility function
          */
-        it("Test table validator function", () => {
-            for (const t of dt.tables) {
-                chai.assert.isTrue(dt.validTable($(t)), "Failed validation on " + t.id);
+        it("Test table validator function", function () {
+            for (const t of this.dt.tables) {
+                chai.assert.isTrue(this.dt.validTable($(t)), "Failed validation on " + t.id);
             }
         });
 
         /**
          * Check DataTable lib loads correctly on all candidate tables
          */
-        it("Check DataTable loads correctly on all candidates", () => {
+        it("Check DataTable loads correctly on all candidates", function () {
             /** if DataTables lib applied on table correctly, then the <table> node has a new class "dataTable" */
-            for (const t of dt.tables) {
+            for (const t of this.dt.tables) {
                 chai.assert.include(
                     t.getAttribute("class"),
                     "dataTable",
@@ -51,9 +47,9 @@ describe("Datatables", () => {
         /**
          * Check if the <table> node is wrapped with a <div>, created to facilitate CSS customization
          */
-        it("Check table is wrapped", () => {
+        it("Check table is wrapped", function () {
             // Table node must be wrapped on a div.SigTools__dt__table
-            for (const t of dt.tables) {
+            for (const t of this.dt.tables) {
                 chai.assert.equal(
                     t.parentElement.getAttribute("class"),
                     "SigTools__dt__table",
@@ -65,9 +61,9 @@ describe("Datatables", () => {
         /**
          * Check if <table> node and all DataTable elements (filters, buttons) are wrapped in a <div> element
          */
-        it("Check table and DataTable are wrapped", () => {
+        it("Check table and DataTable are wrapped", function () {
             // Table node and parent DataTable nodes must be wrapped on a div.SigTools__dt__table
-            for (const t of dt.tables) {
+            for (const t of this.dt.tables) {
                 chai.assert.equal(
                     t.parentElement.parentElement.getAttribute("class"),
                     "SigTools__dt",
@@ -77,10 +73,10 @@ describe("Datatables", () => {
         });
     });
 
-    describe("Single row feature (user setting)", () => {
-        it("Single row not disabled", () => {
+    describe("Single row feature (user setting)", function () {
+        it("Single row not disabled", function () {
             updatejQueryContext("test/pages/datatables/single_row.html").then(() => {
-                dt = new DataTable();
+                const dt = new DataTable();
                 dt.disable_one_row = false;
                 for (const t of dt.tables) {
                     chai.assert.isTrue(dt.validTable($(t)));
@@ -88,9 +84,9 @@ describe("Datatables", () => {
             });
         });
 
-        it("Single row disabled", () => {
+        it("Single row disabled", function () {
             updatejQueryContext("test/pages/datatables/single_row.html").then(() => {
-                dt = new DataTable();
+                const dt = new DataTable();
                 dt.disable_one_row = true;
                 for (const t of dt.tables) {
                     chai.assert.isFalse(dt.validTable($(t)));
@@ -99,27 +95,22 @@ describe("Datatables", () => {
         });
     });
 
-    describe("Test tables with implicit headers", () => {
-        let dt = null;
-
-        /**
-         * Loads sample file with multiple valid tables
-         */
-        before(() => {
+    describe("Test tables with implicit headers", function () {
+        before(function () {
             return new Promise((resolve) =>
                 updatejQueryContext("test/pages/datatables/implicit_headers.html").then(
                     () => {
-                        dt = new DataTable();
+                        this.dt = new DataTable();
                         resolve();
                     }
                 )
             );
         });
 
-        it("Find implicit headers", () => {
+        it("Find implicit headers", function () {
             const customAssert = (tableId, numHeaderRows, msg) => {
                 const $t = $(`#${tableId}`).first();
-                const $h = dt.__getHeaderRows($t);
+                const $h = this.dt.__getHeaderRows($t);
                 chai.assert.strictEqual(
                     $h.length,
                     numHeaderRows,
